91879289a8051a6e56bbf4254a3d0b85286a5c27
nada.h22425 <nada.h22425@gmail.com>
2023-10-19 17:31:51 +0300

    new products

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -5,12 +5,14 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeSliderComponent } from './home-slider/home-slider.component';
 import { MainHomeComponent } from './main-home/main-home.component';
 import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NewProductsComponent } from './new-products/new-products.component';
 
 
 @NgModule({
   declarations: [
     HomeSliderComponent,
-    MainHomeComponent
+    MainHomeComponent,
+    NewProductsComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/modules/home/new-products/new-products.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-products',
+  templateUrl: './new-products.component.html',
+  styleUrls: ['./new-products.component.scss']
+})
+export class NewProductsComponent implements OnInit {
+
+  products=[
+    {image:'assets/images/pic.png',name:'تيشرت كم طول 52 سم',rate:3,option:'new',price:120,oldPrice:150},
+    {image:'assets/images/pic-1.png',name:'تيشرت كم طول 52 سم',rate:3,option:'new',price:110,oldPrice:160},
+    {image:'assets/images/pic-2.png',name:'تيشرت كم طول 52 سم',rate:3,option:'show',price:100,oldPrice:140},
+    {image:'assets/images/pic-3.png',name:'تيشرت كم طول 52 سم',rate:3,option:'new',price:150,oldPrice:170},
+    {image:'assets/images/pic-4.png',name:'تيشرت كم طول 52 سم',rate:3,option:'show',price:140,oldPrice:200},
+    {image:'assets/images/pic-5.png',name:'تيشرت كم طول 52 سم',rate:3,option:'show',price:240,oldPrice:300},
+    {image:'assets/images/pic-6.png',name:'تيشرت كم طول 52 سم',rate:3,option:'new',price:140,oldPrice:200},
+  ]
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
